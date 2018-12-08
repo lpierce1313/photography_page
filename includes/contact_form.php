ea0df3e9b47024abe873e3fb69e1c197da79bebf
Luke Pierce <lpierce1313@gmail.com>
2018-12-07 18:34:45 -0700

    Change this to a meaningful description

--- includes/contact_form.php
+++ includes/contact_form.php
@@ -2,11 +2,11 @@
 if(isset($_POST['user_name'])){
 	$name 		= $_POST['user_name'];
 	$email 		= $_POST['user_email'];
-	$to 		= 'email@gmail.com';
+	$to 		= 'lpierce1313@gmail.com';
 	$subject 	= $_POST['message_subject'];
 	$message 	= $_POST['message_content'];
 	$headers 	= 'From: '. $name .' <'.$email.'>';
-	
+
 	if(mail($to, $subject, $message, $headers)){
 		echo 'success';
 	}
